9e5f9c17eb4d69756857700e5a38281513defb5d
lavantrinh <lavantrinh@gmail.com>
2023-04-11 17:59:23 +0900

    up

---
+++ components/Equipments/EquipmentContent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function EquipmentContent(props) {
+    return (
+        <div>
+          <h1>noi dung</h1>
+        </div>
+    );
+}
+
+export default EquipmentContent;
--- pages/equipment.js
+++ pages/equipment.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Header from '../components/Header';
 import NavTop from '../components/NavTop';
+import { Col, Container, Row } from 'react-bootstrap';
+import LeftSideBar from '../components/Equipments/LeftSideBar';
+import EquipmentContent from '../components/Equipments/EquipmentContent';
 
 function equipment(props) {
     return (
         <>
           <Header/>
           <NavTop active="equipment"/>
-<h2 className='pt-5 mt-5'>cxin</h2>
+<Container className='mt-5 pt-5'>
+  <Row>
+    <Col xl={3} lg={3}>
+    <LeftSideBar/>
+    </Col>
+    <Col xl={9} lg={9}>
+    <EquipmentContent/>
+    </Col>
+  </Row>
+</Container>
         </>
     );
 }
